0cd90972c4205fb934d7ff6c523fd0a25946e277
Markus Mittendrein <git@maxmitti.tk>
2019-09-15 23:51:46 +0200

    Initial

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install debootstrap -y
+mkdir chroot
+debootstrap --arch=i386 --components=main,universe,multiverse bionic chroot http://us-central1.gce.archive.ubuntu.com/ubuntu
+
+./setup_chroot.sh
+chroot chroot apt-get update
+chroot chroot apt-get install -y software-properties-common
+chroot chroot add-apt-repository -y ppa:ubuntu-toolchain-r/test
+chroot chroot apt-get install -y gcc g++ gcc-9 g++-9
+chroot chroot update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 1
+chroot chroot update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 1
+
+./unmount_chroot.sh
+tar czf chroot.tar.gz chroot
