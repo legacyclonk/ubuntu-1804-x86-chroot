27e24dc45312a36a19035be8680da4b92c3adcd4
Markus Mittendrein <git@maxmitti.tk>
2019-09-17 20:10:30 +0200

    Optimizations

--- build.sh
+++ build.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-set -e
+set -ex
 
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install debootstrap -y
+mkdir ramdisk
+mount -t tmpfs tmpfs ramdisk
+cd ramdisk
+
 mkdir chroot
-debootstrap --arch=i386 --components=main,universe,multiverse bionic chroot http://us-central1.gce.archive.ubuntu.com/ubuntu
+DEBIAN_FRONTEND=noninteractive apt-get install debootstrap -y
+debootstrap --arch=i386 --components=main,universe,multiverse --variant=minbase --include=zlib1g-dev,libgl1-mesa-dev,libjpeg-dev,libpng-dev,libssl-dev,libglu1-mesa-dev,libglew-dev,libsdl1.2-dev,libsdl-mixer1.2-dev,libfreetype6-dev,freeglut3-dev,libxpm-dev,cmake,make,git,software-properties-common --exclude=init,init-system-helpers,initramfs-tools,initramfs-tools-bin,initramfs-tools-core, bionic chroot http://us-central1.gce.archive.ubuntu.com/ubuntu 
 
-./setup_chroot.sh
-chroot chroot apt-get update
-chroot chroot apt-get install -y software-properties-common
+../setup_chroot.sh
 chroot chroot add-apt-repository -y ppa:ubuntu-toolchain-r/test
-chroot chroot apt-get install -y gcc zlib1g-dev build-essential libgl1-mesa-dev libjpeg-dev libpng-dev libssl-dev libglu1-mesa-dev libglew-dev libsdl1.2-dev libsdl-mixer1.2-dev libfreetype6-dev freeglut3-dev libxpm-dev cmake make git g++ gcc-9 g++-9
+chroot chroot apt-get install --no-install-recommends -y gcc-9 g++-9
 chroot chroot update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 1
 chroot chroot update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 1
 
-./unmount_chroot.sh
-tar czf chroot.tar.gz chroot
+../unmount_chroot.sh
+tar cJf chroot.tar.xz chroot
