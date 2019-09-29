c24f8eef61ba803b235229b7683373c5b65474d9
Markus Mittendrein <git@maxmitti.tk>
2019-09-29 23:31:01 +0200

    Add libgtk2.0-dev

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cd ramdisk
 
 mkdir chroot
 DEBIAN_FRONTEND=noninteractive apt-get install debootstrap -y
-debootstrap --arch=i386 --components=main,universe,multiverse --variant=minbase --include=zlib1g-dev,libgl1-mesa-dev,libjpeg-dev,libpng-dev,libssl-dev,libglu1-mesa-dev,libglew-dev,libsdl1.2-dev,libsdl-mixer1.2-dev,libfreetype6-dev,freeglut3-dev,libxpm-dev,cmake,make,git,software-properties-common --exclude=init,init-system-helpers,initramfs-tools,initramfs-tools-bin,initramfs-tools-core, bionic chroot http://us-central1.gce.archive.ubuntu.com/ubuntu 
+debootstrap --arch=i386 --components=main,universe,multiverse --variant=minbase --include=zlib1g-dev,libgl1-mesa-dev,libjpeg-dev,libpng-dev,libssl-dev,libglu1-mesa-dev,libglew-dev,libsdl1.2-dev,libsdl-mixer1.2-dev,libfreetype6-dev,freeglut3-dev,libxpm-dev,cmake,make,git,software-properties-common,libgtk2.0-dev --exclude=init,init-system-helpers,initramfs-tools,initramfs-tools-bin,initramfs-tools-core bionic chroot http://us-central1.gce.archive.ubuntu.com/ubuntu 
 
 ../setup_chroot.sh
 chroot chroot add-apt-repository -y ppa:ubuntu-toolchain-r/test
