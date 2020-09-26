208828887241ae61f90b27ec1e4f09fef0451bb5
Markus Mittendrein <git@maxmitti.tk>
2020-09-26 22:18:00 +0200

    Use kitware's PPA for cmake

--- build.sh
+++ build.sh
@@ -7,11 +7,14 @@ cd ramdisk
 
 mkdir chroot
 DEBIAN_FRONTEND=noninteractive apt-get install debootstrap -y
-debootstrap --arch=i386 --components=main,universe,multiverse --variant=minbase --include=sudo,zlib1g-dev,libgl1-mesa-dev,libjpeg-dev,libpng-dev,libssl-dev,libglu1-mesa-dev,libglew-dev,libsdl1.2-dev,libsdl-mixer1.2-dev,libfreetype6-dev,freeglut3-dev,libxpm-dev,cmake,make,git,software-properties-common,libgtk2.0-dev --exclude=init,init-system-helpers,initramfs-tools,initramfs-tools-bin,initramfs-tools-core bionic chroot http://us-central1.gce.archive.ubuntu.com/ubuntu
+debootstrap --arch=i386 --components=main,universe,multiverse --variant=minbase --include=sudo,zlib1g-dev,libgl1-mesa-dev,libjpeg-dev,libpng-dev,libssl-dev,libglu1-mesa-dev,libglew-dev,libsdl1.2-dev,libsdl-mixer1.2-dev,libfreetype6-dev,freeglut3-dev,libxpm-dev,cmake,make,git,software-properties-common,libgtk2.0-dev,gnupg,gpg-agent --exclude=init,init-system-helpers,initramfs-tools,initramfs-tools-bin,initramfs-tools-core bionic chroot http://us-central1.gce.archive.ubuntu.com/ubuntu
 
 ../setup_chroot.sh
+curl -L https://apt.kitware.com/keys/kitware-archive-latest.asc | chroot chroot apt-key add -
+chroot chroot apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
 chroot chroot add-apt-repository -y ppa:ubuntu-toolchain-r/test
-chroot chroot apt-get install --no-install-recommends -y gcc-10 g++-10
+chroot chroot apt-get update
+chroot chroot apt-get install --no-install-recommends -y gcc-10 g++-10 cmake
 chroot chroot update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100 --slave /usr/bin/g++ g++ /usr/bin/g++-10 --slave /usr/bin/gcov gcov /usr/bin/gcov-10
 
 ../unmount_chroot.sh
