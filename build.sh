c2a1922002bcfdab4b58ad5a7829b6cbcb852cca
Markus Mittendrein <maxmitti@maxmitti.tk>
2022-05-19 21:01:58 +0200

    Add unzip and 7z

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cd ramdisk
 
 mkdir chroot
 DEBIAN_FRONTEND=noninteractive apt-get install debootstrap -y
-debootstrap --arch=i386 --components=main,universe,multiverse --variant=minbase --include=sudo,zlib1g-dev,libgl1-mesa-dev,libjpeg-dev,libpng-dev,libssl-dev,libglu1-mesa-dev,libglew-dev,libsdl1.2-dev,libsdl-mixer1.2-dev,libfreetype6-dev,freeglut3-dev,libxpm-dev,cmake,make,git,software-properties-common,libgtk2.0-dev,gnupg,gpg-agent --exclude=init,init-system-helpers,initramfs-tools,initramfs-tools-bin,initramfs-tools-core bionic chroot http://us-central1.gce.archive.ubuntu.com/ubuntu
+debootstrap --arch=i386 --components=main,universe,multiverse --variant=minbase --include=sudo,zlib1g-dev,libgl1-mesa-dev,libjpeg-dev,libpng-dev,libssl-dev,libglu1-mesa-dev,libglew-dev,libsdl1.2-dev,libsdl-mixer1.2-dev,libfreetype6-dev,freeglut3-dev,libxpm-dev,cmake,make,git,software-properties-common,libgtk2.0-dev,gnupg,gpg-agent,unzip,p7zip --exclude=init,init-system-helpers,initramfs-tools,initramfs-tools-bin,initramfs-tools-core bionic chroot http://us-central1.gce.archive.ubuntu.com/ubuntu
 
 ../setup_chroot.sh
 curl -L https://apt.kitware.com/keys/kitware-archive-latest.asc | chroot chroot apt-key add -
